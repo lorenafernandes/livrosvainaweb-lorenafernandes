391f5192aa4dc33ddaec9df63c6e4b274310b85b
Lorena Fernandes <lorena.kailany.fernandes@gmail.com>
2024-11-29 19:22:25 -0300

    Criando 'Livros Doados'

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,8 +1,23 @@
+import s from '../livrosDoados/livrosDoados.module.scss'
+import livroProtagonista from '../../assets/livroProtagonista.png'
+
 export default function LivrosDoados(){
     return(
         <main>
-            <section>
-                <p>Eu sou LivrosDoados</p>
+            <section className={s.livrosDoadosSection}>
+                <h2>Livros Doados</h2>
+
+                <section className={s.containerCards}>
+                    <section>
+                        <img src={livroProtagonista} alt='Imagem da capa do livro O Protagonista' />
+
+                        <div>
+                            <h3>O protagonista</h3>
+                            <p>Susanne Andrade</p>
+                            <p>Ficção</p>
+                        </div>
+                    </section>
+                </section>
             </section>
         </main>
     )
